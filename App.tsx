8ffcdc3e0c7ef9da7ab7bdcc73c3404807c8a87e
tupa4kka1 <niilo.tanhuala@gmail.com>
2026-01-14 20:06:32 +0200

    vikat koodinpätkät

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Modal} from "react-native";
+
 
 export default function App() {
+
+  const [showModal, toggleModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Modal visible={showModal}>
+        <View>
+            <View style={{alignItems: 'center', marginTop:200, backgroundColor:"lightgrey", padding: 20}}>
+              <Text style={{textAlign: 'center'}} >This is the modal ahahahahh</Text>
+              <Pressable onPress={() => toggleModal(!showModal)}>
+                <Text style={{padding: 5, fontWeight: 'bold', fontSize: 20, marginTop:50}}>Close</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+      <Pressable onPress = {() => toggleModal(!showModal)}>
+      <Text>Press to show modal</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
+}
+// </Pressable>Text>Modal visible : {showModal ? "true" : "false"}</Text>
+function showmodal() {
+  return 1
 }
 
 const styles = StyleSheet.create({
